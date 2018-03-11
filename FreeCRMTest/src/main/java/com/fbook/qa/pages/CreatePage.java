581c7fcfabefc355dc711ac6e84808e8799f697a
anjusri4 <anjusri4@gmail.com>
2018-03-11 17:32:07 +0530

    updated with code

---
+++ FreeCRMTest/src/main/java/com/fbook/qa/pages/CreatePage.java
@@ -0,0 +1,7 @@
+package com.fbook.qa.pages;
+
+import com.fbook.testbase.TestBase;
+
+public class CreatePage  extends TestBase {
+
+}
---
+++ FreeCRMTest/src/main/java/com/fbook/util/TestUtil.java
@@ -0,0 +1,7 @@
+package com.fbook.util;
+
+public class TestUtil { 
+	public static long PAGE_LOAD_TIMEOUT =20;
+	public static long IMPLICT_WAIT =10;
+
+}
